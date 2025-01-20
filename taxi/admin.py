347fdf8900d5b067803642ffc6811e30a7ef0190
Dmytro <dima.sichkar2003@gmail.com>
2025-01-20 09:36:18 +0200

    Adding files

---
+++ taxi/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from taxi.models import Driver, Manufacturer, Car
+
+@admin.register(Driver)
+class DriverAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("license_number",)
+    fieldsets = UserAdmin.fieldsets + (("Additional info", {"fields": ("licence_number",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (("Additional info", {"fields": ("licence_number",)}),)
+
+
+@admin.register(Car)
+class CarAdmin(admin.ModelAdmin):
+    list_filter = ("manufacturer",)
+    search_filter = ("title",)
+
+admin.site.register(Manufacturer)
